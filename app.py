10264fe1a1d7c3230ae6a1a5fa90a6c07e76693d
vasundharak1 <cutevarsha2011@gmail.com>
2021-07-22 18:51:38 +0000

    added a lot of features

--- app.py
+++ app.py
@@ -11,6 +11,26 @@ app = Flask(__name__)
 
 # -- Routes section --
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index():
-    return render_template('hello is it working')
+    return render_template('index.html')
+
+@app.route('/smart.html')
+def smart():
+    return render_template('smart.html')
+
+@app.route('/timeline.html')
+def timeline():
+    return render_template('timeline.html')
+
+@app.route('/info.html')
+def info():
+    return render_template('info.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+# @app.route('/profile.html')
+# def profile():
+#     return render_template('profile.html')
